516a7b6f3317199629f778d676f16aedde5036a8
Jmarlow <45525109+Jemarlow@users.noreply.github.com>
2022-12-09 02:24:32 +0000

    model fix

--- things/models.py
+++ things/models.py
@@ -3,7 +3,6 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 class Thing(models.Model):
-    name = models.CharField(unique=True, blank=False, max_length=30),
-    description = models.CharField(unique=False, blank=True, max_length=120),
-    quantity = models.PositiveSmallIntegerField(unique=False, default=1,
-         validators=[MaxValueValidator(100), MinValueValidator(0)])
+    name = models.CharField(unique=True, blank=False, max_length=30)
+    description = models.CharField(unique=False, blank=True, max_length=120)
+    quantity = models.PositiveSmallIntegerField(unique=False, default=1, validators=[MaxValueValidator(100), MinValueValidator(0)])
